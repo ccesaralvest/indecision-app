4d3fae5f22437c76cc929bf3547cd215092e934e
ccesar <ccesar@MacBook-Pro-de-ccesar.local>
2019-06-08 14:22:46 -0300

    Conditions JSX - Testing Variables before render

--- src/app.js
+++ src/app.js
@@ -1,16 +1,45 @@
-console.log("APP JS - INIT")
 
-// JSX
+var appRoot = document.getElementById('app');
+
+// set variables to 'header page'
+var app = {
+    title: "Indecision App",
+    subtitle: "Blá Blá Blá",
+    options: ['One', 'Two']
+};
 var template = (
     <div>
-        <h1>Indecision App</h1>
-        <p>This is some info</p>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? "Here are your options" : "No Options"}</p>
         <ol>
             <li>Item onde</li>
             <li>Item two</li>
         </ol>
     </div>
 );
-var appRoot = document.getElementById('app');
+
+// object like Json comes
+var user = {
+    name: "Caio Alves",
+    age: 29
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+};
+
+var templateTwo = (
+    <div>
+        <h1>User Name: {user.name ? user.name : "anonymous"}</h1>
+        <p>Age: { ( user.age && user.age >= 29 )   && user.age}</p>
+        {getLocation(user.location)}
+    </div>
+);
+
+
+
 
 ReactDOM.render(template, appRoot);
